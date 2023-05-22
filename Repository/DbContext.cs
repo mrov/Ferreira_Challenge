b963264885d2f676a95dfb2af9d5516a437faa04
Moabe Renato <mrov@cin.ufpe.br>
2023-05-22 10:49:39 -0300

    repository tests and fix in dbcontext

--- Repository/DbContext.cs
+++ Repository/DbContext.cs
@@ -4,16 +4,8 @@ using Models;
 
 public class MyDbContext : DbContext
 {
-    private readonly IConfiguration configuration;
-    public MyDbContext(DbContextOptions<MyDbContext> options, IConfiguration configuration) : base(options)
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
     {
-        this.configuration = configuration;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        string connectionString = configuration.GetConnectionString("DefaultConnection");
-        optionsBuilder.UseOracle(connectionString);
     }
 
     // DbSet properties representing your database tables
